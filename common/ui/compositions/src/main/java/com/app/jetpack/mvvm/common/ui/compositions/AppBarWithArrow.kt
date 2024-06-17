e4bc9c9052d416bc8a7db47113af370c96e17497
Rajput Vijay <vijaybhai.rajput@robosoftin.com>
2024-06-17 09:06:39 +0530

    - Resolved app bar toolbar issue

--- common/ui/compositions/src/main/java/com/app/jetpack/mvvm/common/ui/compositions/AppBarWithArrow.kt
+++ common/ui/compositions/src/main/java/com/app/jetpack/mvvm/common/ui/compositions/AppBarWithArrow.kt
@@ -3,11 +3,7 @@ package com.app.jetpack.mvvm.common.ui.compositions
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,10 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,37 +24,30 @@ fun AppBarWithArrow(
     title: String?,
     pressOnBack: () -> Unit
 ) {
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    //val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     TopAppBar(
         title = {
-            Row {
-                Spacer(modifier = Modifier.width(10.dp))
-
-                Image(
-                    imageVector = Icons.Filled.ArrowBack,
-                    colorFilter = ColorFilter.tint(Color.White),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .clickable {
-                            pressOnBack()
-                        }
-                )
-
-                Spacer(modifier = Modifier.width(12.dp))
-
-                Text(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.CenterVertically),
-                    text = title ?: "",
-                    style = MaterialTheme.typography.titleLarge,
-                    color = Color.White
-                )
-            }
+            Text(
+                modifier = Modifier
+                    .padding(8.dp),
+                text = title ?: "",
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
         },
-        scrollBehavior = scrollBehavior,
-        modifier = Modifier.height(58.dp)
+        navigationIcon = {
+            Image(
+                imageVector = Icons.Filled.ArrowBack,
+                colorFilter = ColorFilter.tint(Color.White),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable {
+                        pressOnBack()
+                    }
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.primary),
     )
 }
